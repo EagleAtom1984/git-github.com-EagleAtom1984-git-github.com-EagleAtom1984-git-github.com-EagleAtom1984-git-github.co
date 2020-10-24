44d777d55b8aaa7c56a8a10b4d741fa4b768edde
Adan Newman <Adan2.Newman@live.uwe.ac.uk>
2020-10-24 19:51:04 +0100

    Games Console summary
    Making more progress

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    int guess;
+    std::cout << "Enter your estimate: ";
+
+    std::srand(std::time(0));//use the current time as seed for random generator
+    int random_variable = std::rand();
+    std::cout << "Random value on [0 " << RAND_MAX << "]: "
+              << random_variable << '\n';
+
     return 0;
 }
