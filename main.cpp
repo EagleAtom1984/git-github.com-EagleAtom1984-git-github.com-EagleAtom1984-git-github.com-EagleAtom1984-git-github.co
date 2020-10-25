da02d47ec06dc1138969bbd41beb4aab8dfa60b4
Adan Newman <Adan2.Newman@live.uwe.ac.uk>
2020-10-25 22:46:33 +0000

    Worksheet 1 Part 4
    I have made progress, i have something developed now i need to fix it

--- main.cpp
+++ main.cpp
@@ -6,12 +6,24 @@ int main() {
 
     int secretNum = 4;
     int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    bool outOfGuesses = false;
 
-    while(secretNum != guess){
-    std::cout << "Enter guess: ";
-    std::cin >> guess;
+    while(secretNum != guess && !outOfGuesses) {
+        if(guessCount < guessLimit) {
+            std::cout << "Enter guess: ";
+            std::cin >> guess;
+            guessCount++;
+        } else {
+            outOfGuesses = true;
+        }
     }
-
+    if(outOfGuesses) {
+        std::cout << "You lose would you like to try again?";
+    } else {
+        std::cout << "Well done!";
+    }
     std::srand(std::time(0));//use the current time as seed for random generator
     int random_variable = std::rand();
     std::cout << "Random value on [0 " << RAND_MAX << "]: "
